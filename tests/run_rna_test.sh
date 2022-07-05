9d08d91ddd96ad9853468e67f08d6ddd6e0a5fa2
Michael Kotliar <misha.kotliar@gmail.com>
2022-07-05 18:05:39 -0400

    Make scRNA tests separate from Multiome

--- tests/run_rna_test.sh
+++ tests/run_rna_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-INPUTS_DIR="${BASE_DIR}/data/inputs"
+INPUTS_DIR="${BASE_DIR}/data/rna_inputs"
 TOOLS_DIR="${BASE_DIR}/../tools"
 JOBS_DIR=$BASE_DIR
 
@@ -12,22 +12,22 @@ wget -q --show-progress -O aggregation.csv https://figshare.com/ndownloader/file
 wget -q --show-progress -O condition.csv https://figshare.com/ndownloader/files/34819519
 wget -q --show-progress -O mouse_cell_cycle_genes.csv https://figshare.com/ndownloader/files/34822054
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_rna_filter"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_rna_filter"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo "Running sc-rna-filter.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-rna-filter.cwl" "$JOBS_DIR/sc-rna-filter.yaml"
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_rna_reduce"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_rna_reduce"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo "Running sc-rna-reduce.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-rna-reduce.cwl" "$JOBS_DIR/sc-rna-reduce.yaml"
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_rna_cluster"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_rna_cluster"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo "Running sc-rna-cluster.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-rna-cluster.cwl" "$JOBS_DIR/sc-rna-cluster.yaml"
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_ctype_assign"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_ctype_assign"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo -e "cluster\tctype" > cell_types.tsv
 for i in {0..18}
@@ -37,12 +37,12 @@ done
 echo "Running sc-ctype-assign.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-ctype-assign.cwl" "$JOBS_DIR/sc-ctype-assign.yaml"
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_rna_de_pseudobulk"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_rna_de_pseudobulk"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo "Running sc-rna-de-pseudobulk.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-rna-de-pseudobulk.cwl" "$JOBS_DIR/sc-rna-de-pseudobulk.yaml"
 
-WORKING_DIR="${BASE_DIR}/data/outputs/sc_rna_da_cells"
+WORKING_DIR="${BASE_DIR}/data/rna_outputs/sc_rna_da_cells"
 mkdir -p $WORKING_DIR && cd $WORKING_DIR
 echo "Running sc-rna-da-cells.cwl in ${WORKING_DIR} directory."
 cwltool --debug "$TOOLS_DIR/sc-rna-da-cells.cwl" "$JOBS_DIR/sc-rna-da-cells.yaml"
